655199e2372def8a56997907885efcc833dfdc2f
Thanh Truong <dagnar@gmail.com>
2015-07-19 15:29:56 -0700

    small table tweaks

--- web.js
+++ web.js
@@ -1,7 +1,19 @@
-var gzippo = require('gzippo');
-var express = require('express');
+var express = require("express");
 var app = express();
+app.use(express.static(__dirname+"/dist"));
+app.get("/", function(req, res){
+  res.sendfile("/index.html");
+});
+var port = process.env.PORT || 9000;
+app.listen(port, function() {
+    console.log("Listening on port " + port);
+});
+
+
+// var gzippo = require('gzippo');
+// var express = require('express');
+// var app = express();
  
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+// app.use(express.logger('dev'));
+// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// app.listen(process.env.PORT || 5000);
